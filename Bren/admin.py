88c0d4ddb165d95af568fb3c77faa0038d336587
Wade <wdh@uvic.ca>
2009-11-05 19:37:00 -0800

    Adds remaining files

---
+++ Bren/admin.py
@@ -0,0 +1,74 @@
+from Crossfit.Bren.models import *
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+ 
+class Variation_usedInline(admin.StackedInline):
+    model = Variation_used
+    
+class Workout_elementInline(admin.StackedInline):
+    model = Element_used 
+
+class Element_usedAdmin(admin.ModelAdmin):    
+    list_display = ('workout', 'element','reps')
+    fields = ['workout', 'element', 'reps']
+    list_filter = ['workout', 'element']
+
+class Workout_classAdmin(admin.ModelAdmin):    
+    list_display = ('date', 'workout', 'class_info')
+    fields = ['date', 'workout', 'class_info']
+    list_filter = ['date', 'workout', 'class_info']
+
+class Completed_workoutAdmin(admin.ModelAdmin):
+    list_display = ('user', 'workout_class', 'mins', 'secs', 'rounds')
+    fields = ['user', 'workout_class', 'mins', 'secs', 'rounds']
+    list_filter = ['user', 'workout_class', 'mins', 'rounds']
+    inlines = [Variation_usedInline]
+
+    def day_of_week(self, completed_workout):
+        return get_weekday(completed_workout.date.weekday())
+    
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('element', 'name')
+    list_filter = ['element']
+
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ('name', 'comments', 'workout_type', 'time', 'rounds')
+    fields = ['name', 'comments', 'workout_type', 'time', 'rounds']
+    inlines = [Workout_elementInline]
+
+class Variation_usedAdmin(admin.ModelAdmin):
+     list_display = ('completed_workout', 'variation')
+     list_filter = ['variation']
+
+     def user_name(self, variation):
+         return variation.completed_workout.user
+
+     def workout_name(self, workout):
+         return workout.completed_workout.workout
+        
+     def workout_date(self, variation):
+         return variation.completed_workout.date
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+class UserAdmin(UserAdmin):
+    inlines = [UserProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Workout_type)
+admin.site.register(Class_info)
+admin.site.register(Workout_class, Workout_classAdmin)
+admin.site.register(Completed_workout, Completed_workoutAdmin)
+admin.site.register(Element)
+admin.site.register(Element_used, Element_usedAdmin)
+admin.site.register(Workout, WorkoutAdmin)
+admin.site.register(Variation, VariationAdmin)
+admin.site.register(Variation_used, Variation_usedAdmin)
+
+
+
+
+
---
+++ Bren/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib import admin
+import datetime
+from django.contrib.auth.models import User
+from Crossfit.Bren.calcs import*
+
+   
+class Workout_type(models.Model):
+    name = models.CharField(max_length=20)
+    def __unicode__(self):
+        return self.name
+
+class Workout(models.Model):
+    name = models.CharField(max_length=20)
+    comments = models.CharField(max_length=200)
+    workout_type = models.ForeignKey(Workout_type)
+    time = models.IntegerField()
+    rounds = models.IntegerField()
+    def __unicode__(self):
+        return self.name
+
+class Class_info(models.Model):
+    title = models.CharField(max_length=30)
+    def __unicode__(self):
+        return self.title
+    
+class Workout_class(models.Model):
+    date = models.DateField('Date Completed')
+    workout = models.ForeignKey(Workout)
+    class_info = models.ForeignKey(Class_info)
+    def __unicode__(self):
+        return self.workout.name+ " "+self.class_info.title + " " +self.date.isoformat()
+
+class Completed_workout(models.Model):
+    mins = models.IntegerField()
+    secs = models.IntegerField()
+    user = models.ForeignKey(User)
+    workout_class = models.ForeignKey(Workout_class)
+    rounds = models.IntegerField()
+
+    def __unicode__(self):
+        return self.user.username+ ", "+ self.workout_class.workout.name
+
+    def get_month(self):
+        return get_month(self.workout_class.date.month)
+        
+    def get_weekday(self):
+        return get_weekday(self.workout_class.date.weekday())
+    
+class Element(models.Model):
+    name = models.CharField(max_length=20)
+    def __unicode__(self):
+        return self.name
+
+class Element_used(models.Model):
+    workout = models.ForeignKey(Workout)
+    element = models.ForeignKey(Element)
+    reps = models.IntegerField()
+    def __unicode__(self):
+        return self.workout.name+ ", "+ self.element.name
+
+class Variation(models.Model):
+    name = models.CharField(max_length=20)
+    element = models.ForeignKey(Element)
+    def __unicode__(self):
+        return self.element.name+ ", "+ self.name
+
+class Variation_used(models.Model):
+    completed_workout= models.ForeignKey(Completed_workout)
+    variation= models.ForeignKey(Variation)
+    def __unicode__(self):
+        return self.completed_workout.user.username+ ", "+self.completed_workout.workout_class.workout.name+  ", "+ self.variation.name
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    
+    
+
+
+    
