8cb9b99c06dbed873db3daeb3218609ea5c3f2be
owen <owenmead@gmail.com>
2009-11-21 10:36:55 -0800

    Updates views and renames some methods

--- Bren/views.py
+++ Bren/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render_to_response
 from django import forms
 import Crossfit.Bren.models as model
 
-class Crazy(forms.Form):
+class WorkoutForm(forms.Form):
     name = forms.CharField(max_length=20)
 
-    def __init__(self, *args, **kw):
+    def __init__(self, elements, *args, **kw):
         super(Crazy, self).__init__(*args, **kw)
-        for i in range(3):
+        for i, field_dict in enumerate(elements):
+
+
+
             self.fields['extra_info_%d' % i] = forms.CharField(max_length=20)
 
 def index(request):
@@ -26,9 +29,11 @@ def index(request):
     #}
 
     api_data = model.get_workout(1, 1)
+    the_form = WorkoutForm(api_data['elements'])
     data = {
         'name':     api_data['name'],
         'comments': api_data['comments'],
+        'the_form': the_form,
     }
 
     return render_to_response('base.html', data)
