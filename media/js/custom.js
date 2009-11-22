0b802b510dc2f3bd030410a60d9b829aa6c50a55
owen <owenmead@gmail.com>
2009-11-22 15:15:48 -0800

    Updates the numpad JQuery Widget

--- media/js/custom.js
+++ media/js/custom.js
@@ -4,18 +4,53 @@ jQuery.fn.numPadInput = function() {
         var input_field_id = this.id;
         var is_open = false;
 
+        $("#"+input_field_id).addClass("numpad_input_field");
+
+        var closeNumpad = function() {
+            $("#"+num_pad_id).remove();
+            is_open = false;
+        }
+
+        var addNumber = function(number) {
+            var theVal = $("#"+input_field_id).val();
+            // Normalize the input
+            theVal = parseInt(theVal.replace(":", "").replace(/^0*/, "") + number);
+            minutes = parseInt(theVal / 100);
+            seconds = theVal % 100;
+            if (seconds < 10) seconds = "0" + seconds
+            $("#"+input_field_id).val( minutes + ":" + seconds );
+        }
+
         // ====================================
         // = When the User Focus On The Input =
         // ====================================
         $(this).focus(function() {
             if (!is_open) {
-                var numPadHTML = '<div id="'+num_pad_id+'"><div>';
-                numPadHTML += "Input Time"
+                var numPadHTML = '<div id="'+num_pad_id+'" class="numpad_input_container"><div>';
+                numPadHTML += "<table><tbody>";
+                numPadHTML += "<tr><td>1</td><td>2</td><td>3</td></tr>";
+                numPadHTML += "<tr><td>4</td><td>5</td><td>6</td></tr>";
+                numPadHTML += "<tr><td>7</td><td>8</td><td>9</td></tr>";
+                numPadHTML += "<tr><td>C</td><td>0</td><td>OK</td></tr>";
+                numPadHTML += "</tbody></table>";
                 numPadHTML += "</div></div>";
 
                 $(this).after(numPadHTML);
                 is_open = true;
             }
+
+            $("#"+num_pad_id + " > div > table > tbody > tr > td ").each(function() {
+                $(this).click(function() {
+                    if (this.innerHTML == "C") {
+                        $("#"+input_field_id).val("0:00");
+                    } else if (this.innerHTML == "OK") {
+                        closeNumpad();
+                    } else {
+                        addNumber(this.innerHTML);
+                        //$("#"+input_field_id).val( $("#"+input_field_id).val() + this.innerHTML);
+                    }
+                })
+            })
         });
 
 
@@ -27,8 +62,7 @@ jQuery.fn.numPadInput = function() {
             if ($(e.target).parents().filter("#"+num_pad_id).length > 0 || $(e.target).is("#"+input_field_id)) {
                 // pass
             } else {
-                $("#"+num_pad_id).remove();
-                is_open = false;
+                closeNumpad();
             }
 
         });
