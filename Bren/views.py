fd711c0651d5ec573ad09630c5bb70ec416f8a63
owen <owenmead@gmail.com>
2009-11-22 16:53:15 -0800

    Adds basic information and starts to create dave diction

--- Bren/views.py
+++ Bren/views.py
@@ -35,6 +35,8 @@ def workout_form(request, date_str, class_id):
         'comments':     api_data['comments'],
         'workout_type': api_data['workout_type'],
         'class_name':   api_data['class_name'],
+        'date_str':     date_str,
+        'class_id':     class_id,
         'the_form':     the_form,
     }
 
@@ -42,6 +44,33 @@ def workout_form(request, date_str, class_id):
 
 def save_workout(request):
     print request.POST
+
+    # Deal with rounds and time
+    workout_type = request.POST.get('workout_type')
+    if workout_type == "AMRAP":
+        workout_rounds = request.POST.get('workout_reps')
+        workout_time = 0
+    elif workout_type == "Timed":
+        workout_rounds = 0
+        time_parts = request.POST.get('workout_time').split(":")
+        workout_time = int(time_parts[0]) * 60 + int(time_parts[1])
+
+    # Basic Information
+    date_str = request.POST.get('date_str')
+    class_id = request.POST.get('class_id')
+
+    save_dict = {
+        "time":     workout_time,
+        "rounds":   workout_rounds,
+        "date":     date_str,
+        "class_id": class_id,
+
+        #"variations":       [{"order": <int>, "variation_id": <int>, "element_id": <int> }, ...]
+    }
+
+    print save_dict
+
+
     return render_to_response('save_workout.html')
 
 def no_workout_found(request, date, *args):
