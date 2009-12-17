07243a296d4af1066aeebbfce597f0a0a51df3aa
owen <owenmead@gmail.com>
2009-12-16 16:36:23 -0800

    Adds letter buttons

--- media/js/user_picker.js
+++ media/js/user_picker.js
@@ -1,7 +1,13 @@
 jQuery.fn.userPicker = function() {
     // Create a user picker
     return this.each(function() {
-        var main_buttons_html = '<p>Pick A User</p>';
+        var letter_buttons = ["ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"];
+
+        var main_buttons_html = "";
+        for (i in letter_buttons) {
+            main_buttons_html += "<a href='#'>"+letter_buttons[i]+"</a>";
+        }
+        '<p>Pick A User</p>';
         $(this).html(main_buttons_html);
     });
 }
