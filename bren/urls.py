ea17329906beb57b3b4016270c238c46038068ad
owen <owenmead@gmail.com>
2010-02-09 20:21:21 -0800

    bren now has it's own urls module

---
+++ bren/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    (r'^$', 'crossfit.bren.views.index'),
+    # /workout_form/<date>/<class_id>        /workout_form/2009-11-21/3
+    (r'^workout_form/(\d{4}-\d{2}-\d{2})/(\d+)/$', 'crossfit.bren.views.workout_form'),
+    (r'^workout_form/(\d{4}-\d{2}-\d{2})/None/$', 'crossfit.bren.views.no_workout_found'),
+
+    (r'^save_workout/$', 'crossfit.bren.views.save_workout'),
+
+    (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
+
+    (r'^json_api/$', 'crossfit.bren.views.json_api'),
+)
--- urls.py
+++ urls.py
@@ -6,18 +6,8 @@ import django.contrib.auth.views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', 'crossfit.bren.views.index'),
-    # /workout_form/<date>/<class_id>        /workout_form/2009-11-21/3
-    (r'^workout_form/(\d{4}-\d{2}-\d{2})/(\d+)/$', 'crossfit.bren.views.workout_form'),
-    (r'^workout_form/(\d{4}-\d{2}-\d{2})/None/$', 'crossfit.bren.views.no_workout_found'),
-
-    (r'^save_workout/$', 'crossfit.bren.views.save_workout'),
-
-    (r'^login/$', 'django.contrib.auth.views.login'),
-    (r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
-
-    (r'^json_api/$', 'crossfit.bren.views.json_api'),
     (r'^admin/', include(admin.site.urls)),
+    (r'^',       include(crossfit.bren.urls)),
 )
 
 if settings.DEBUG:
