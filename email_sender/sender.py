6dd49deac5033ed545c21d085047a94cbff38a81
owen <owenmead@gmail.com>
2010-02-28 20:06:34 -0800

    Moves email sender to own app

---
+++ email_sender/sender.py
@@ -0,0 +1,18 @@
+from django.template import loader, Context
+from django.core.mail import EmailMessage
+
+def email_user(user, subject, email_template, data_dict):
+
+    # Manually create the template and context, then render the result
+    t = loader.get_template(email_template)
+    c = Context(data_dict)
+    html_content = t.render(c)
+
+    # TODO : Register a better username
+    from_email   = "owenmead_server@owenmead.webfactional.com"
+    to_address = [user.email]
+
+    # Create the actual message
+    msg = EmailMessage(subject, html_content, from_email, to_address)
+    msg.content_subtype = "html"  # Mark the email as an HTML email
+    msg.send()
