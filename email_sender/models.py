5ccfca8bee88647320e49886ac8f43ff401a4a4c
owen <owenmead@gmail.com>
2010-03-08 20:19:31 -0800

    Add UserEmailPermissions to email_sender model

--- email_sender/models.py
+++ email_sender/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from random import random
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.hashcompat import sha_constructor
+
+def gen_subscribe_hash():
+    return sha_constructor(str(random())).hexdigest()[:10]
+
+class UserEmailPermissions(models.Model):
+    subscribe_hash = models.CharField(max_length=20, primary_key=True, default=gen_subscribe_hash)
+
+    user = models.ForeignKey(User, unique=True)
+    has_permission = models.BooleanField(default=False)
