fe8b4e916596e5c0708cdfd898fec99afea1a193
owen <owenmead@gmail.com>
2010-03-08 20:19:56 -0800

    Start to add subscribe_user to email_sender

--- email_sender/sender.py
+++ email_sender/sender.py
@@ -1,6 +1,8 @@
 from django.template import loader, Context
 from django.core.mail import EmailMessage
 
+from crossfit.email_sender.models import UserEmailPermissions
+
 def email_user(user, subject, email_template, data_dict):
 
     # Manually create the template and context, then render the result
@@ -16,3 +18,15 @@ def email_user(user, subject, email_template, data_dict):
     msg = EmailMessage(subject, html_content, from_email, to_address)
     msg.content_subtype = "html"  # Mark the email as an HTML email
     msg.send()
+
+def subscribe_user(user):
+	# Grab existing hash, or generate one
+	email_perms = UserEmailPermissions.objects.filter(user=u)
+	if len(email_perms) > 0:
+		email_perm = email_perms[0]
+	else:
+		email_perm = UserEmailPermissions(user=user)
+	subscribe_hash = email_perm.subscribe_hash
+
+	# Create the email
+	to_address = user.email
