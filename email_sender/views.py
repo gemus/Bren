69bc9fe2287dffccd673b73bed7fdf2ed31e0d04
owen <owenmead@gmail.com>
2010-03-11 19:31:01 -0800

    Sst permission on subscribe code

--- email_sender/views.py
+++ email_sender/views.py
@@ -5,8 +5,9 @@ from crossfit.email_sender.models import UserEmailPermissions
 def confirm(request, given_code):
     try:
         email_perm = UserEmailPermissions.objects.get(pk=given_code)
-        data = dict(given_code=given_code)
-        return render_to_response('email_sender/confirm.html', data)
+        email_perm.has_permission = True
+        email_perm.save()
+        return render_to_response('email_sender/confirm.html')
     except UserEmailPermissions.DoesNotExist:
         data = dict(given_code=given_code)
-        return render_to_response('email_sender/bad_confirm_code.html', data)
+        return render_to_response('email_sender/bad_confirm_code.html')
