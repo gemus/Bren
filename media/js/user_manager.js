560ab20c2a80447512106277ebe16df1ea5b9653
owen <owenmead@gmail.com>
2010-04-09 13:02:05 -0700

    Graphic tweaks to the user manager

--- media/js/user_manager.js
+++ media/js/user_manager.js
@@ -1,24 +1,24 @@
 jQuery.fn.userManager = function(user_name) {
     var container = this;
     var user_name = user_name;
-    
+
     // Create a user picker
     return this.each(function() {
         var get_user_callback = function(result, status) {
             user = result.result;
-            
-            collect  = '<div>';
+
+            collect  = '<div class="name_plate">';
             collect += '<span id="first_name">' + user.first_name + '</span> ';
             collect += '<span id="last_name">'  + user.last_name + '</span>';
             collect += '</div>';
-            collect += '<div>';
+            collect += '<div class="email">';
             collect += '<span id="email">' + user.email + '</span> ';
             collect += '</div>';
-            collect += '<div>';
-            collect += '<span id="last_login">'  + user.last_login + '</span> ';
-            collect += '<span id="date_joined">' + user.date_joined + '</span>';
+            collect += '<div class="dates">';
+            collect += '<label>Last Login</label><span id="last_login">'  + user.last_login + '</span><br>';
+            collect += '<label>Date Joined</label><span id="date_joined">' + user.date_joined + '</span>';
             collect += '</div>';
-            
+
             container.html(collect);
         }
         $.getJSON("/json_api/", {"id": 1,
