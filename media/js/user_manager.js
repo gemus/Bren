1c5c24655f2c3fdc9640c62e0bfb4ad88d0e1f1d
owen <owenmead@gmail.com>
2010-04-16 14:52:47 -0700

    Refactor of the user_manager

--- media/js/user_manager.js
+++ media/js/user_manager.js
@@ -1,161 +1,169 @@
 jQuery.fn.userManager = function(user_name) {
-    var container = this;
-    var user_name = user_name;
+    // If we need options. http://docs.jquery.com/Plugins/Authoring#Options
 
-    var change_view = function(view) {
-        // View One of: 'edit', 'view'
-        if (view == 'view') {
-            $("#view_canvas").show();
-            $("#view_actions").show();
+    // Initalize the userManager
+    return this.each(function() {
+        // Setup the HTML
+        $(this).html('<div style="border: 1px solid #F00;" id="basic_details"></div>');
+        // Create the manager to get the ball rolling
+        new TopManager(user_name).init();
+    });
+}
 
-            $("#edit_canvas").hide();
-            $("#edit_actions").hide();
-            $("#name_plate_error").hide();
-        } else if (view == 'edit') {
-            // Clear old validation errors
-            $("#name_plate_error").hide();
-            $("#email_plate_error").hide();
+// ==============================================
+// = TopManager - Used to coordinate everything =
+// ==============================================
+TopManager = function(user_name) {
+    var self = this;
+    this.user_name = user_name;
 
-            $("#edit_canvas").show();
-            $("#edit_actions").show();
+    // The different sections
+    this.userDetailManager = new UserDetailManager(this, 'basic_details');
+}
 
-            $("#view_canvas").hide();
-            $("#view_actions").hide();
-        }
+TopManager.prototype.init = function() {
+    this.userDetailManager.init();
+}
+
+TopManager.prototype.notify_change = function(section) {
+    // When user details are updated
+    if (section == this.userDetailManager.notify_name) {
+        // Update the scroller
+        var name_tag = this.userDetailManager.user_obj['first_name'] + " " +
+                            this.userDetailManager.user_obj['last_name'];
+        $('#'+this.user_name+'').html(name_tag);
     }
+}
 
+// ===============================================================
+// = UserDetailManager - Manage basic user details (name, email) =
+// ===============================================================
+UserDetailManager = function(manager, canvas_id) {
+    this.notify_name = "user_details"; // Used when notifying others of changes
+    this.manager = manager;
+    this.canvas_id = canvas_id;
+    this.user_obj;
+}
+
+UserDetailManager.prototype.getItem = function(selector_text) {
+    // Return #basic_details selector_text
+    // So sub methods don't have to worry about name conflicts or grabbing wrong elemnts
+    return $('#'+this.canvas_id + " " + (selector_text == undefined ? "" : selector_text));
+}
+
+UserDetailManager.prototype.init = function() {
+    var self = this;
+    // Pull in needed data
+    $.getJSON("/json_api/", {"id": 1,
+                             "method": "get_user",
+                             "params" : JSON.stringify([this.manager.user_name])
+                             },
+                             function(result, status) {
+                                  self.user_obj = result.result;
+                                  self.draw_view();
+                             });
+}
+
+UserDetailManager.prototype.draw_view = function() {
+    var self = this;
+    var view_canvas = '<div class="name_plate">' +
+                          '<span>' + this.user_obj['first_name'] + '</span> '+
+                          '<span>' + this.user_obj['last_name']  + '</span> '+
+                      '</div>'+
+                      '<div class="email_plate">'+
+                          '<a href="mailto:'+ this.user_obj['email']+'">'+
+                              this.user_obj['email'] +
+                          '</a> '+
+                      '</div>'+
+                      '<div class="action_plate">'+
+                          '<a href="javascript:void(0);" id="edit_button">Edit</a>' +
+                      '</div>';
+    this.getItem().html(view_canvas);
+    this.getItem("#edit_button").click(function(){ self.draw_edit(); });
+}
+
+UserDetailManager.prototype.draw_edit = function() {
+    var self = this;
+    var edit_canvas = '<div id="edit_canvas">' +
+                          '<div class="name_plate_edit">'+
+                              '<input type="text" id="first_name" value="' + this.user_obj['first_name'] + '"> '+
+                              '<input type="text" id="last_name" value="'  + this.user_obj['last_name']  + '"> '+
+                          '</div>'+
+                          '<div id="name_plate_error" class="error_plate" style="display: none;"></div>'+
+
+                          '<div class="email_plate_edit">'+
+                              '<input type="text" id="email" value="'  + this.user_obj['email']  + '" style="width: 175px;"> '+
+                          '</div>'+
+                          '<div id="email_plate_error" class="error_plate" style="display: none;"></div>'+
+                      '</div>' +
+                      '<div id="edit_actions">'+
+                          '<a href="javascript:void(0);" id="save_button">Save</a> ' +
+                          '<a href="javascript:void(0);" id="cancel_button">Cancel</a>' +
+                      '</div>';
+
+    this.getItem().html(edit_canvas);
+    this.getItem("#cancel_button").click(function(){ self.draw_view(); });
+    this.getItem("#save_button").click(function(){ self.validate_and_save(); });
+}
+
+UserDetailManager.prototype.validate_and_save = function() {
+    var self = this;
     var validate_email_address = function(email_addy) {
         var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
         return reg.test(email_addy);
     }
 
-    var validate_and_save = function() {
-        var errors = new Array();
-        var first_name_val = $('#first_name_input').val();
-        var last_name_val = $('#last_name_input').val();
-        var email_val = $('#email_input').val();
+    var errors = new Array();
+    var first_name_val = this.getItem("input#first_name").val();
+    var last_name_val  = this.getItem("input#last_name").val();
+    var email_val      = this.getItem("input#email").val();
 
-        // Do some validation
-        if (first_name_val == '') errors.push(['name', 'First Name']);
-        if (last_name_val  == '') errors.push(['name', 'Last Name']);
-        if (!validate_email_address(email_val) && email_val.length > 0 ) {
-            errors.push(['email','Invalid Email']);
-        }
-
-        var success_save_user_callback = function(result, status) {
-            // Update the view canvas
-            $('span#first_name').html(first_name_val);
-            $('span#last_name').html(last_name_val);
-            $('a#email').html(email_val);
-            $('a#email').attr('href', email_val);
-
-            // Then update the scroller
-            $('#'+user_name+'').html(first_name_val + " " + last_name_val);
-
-            // Finally change the view
-            change_view("view");
-        }
-
-        // No Errors so save the user
-        if (errors.length == 0) {
-            $.getJSON("/json_api/", {"id": 1,
-                                     "method": "update_user",
-                                     "params" : JSON.stringify([{'user_name': user_name,
-                                                  'first_name': first_name_val,
-                                                  'last_name' : last_name_val,
-                                                  'email'     : email_val
-                                                 }])
-                                     },
-                                     success_save_user_callback);
-        } else {
-            var name_errors = new Array();
-            var email_errors = new Array();
-            for (i in errors) {
-                if (errors[i][0] == 'name') name_errors.push(errors[i][1]);
-                if (errors[i][0] == 'email') email_errors.push(errors[i][1]);
-            }
-
-            if (name_errors.length > 0) {
-                $("#name_plate_error").slideDown();
-                $("#name_plate_error").html(name_errors.join(", ") + " Is Required");
-            }
-            if (email_errors.length > 0) {
-                $("#email_plate_error").slideDown();
-                $("#email_plate_error").html("Invalid Email");
-            }
-        }
+    // Do some validation
+    if (first_name_val == '') errors.push(['name', 'First Name']);
+    if (last_name_val  == '') errors.push(['name', 'Last Name']);
+    if (!validate_email_address(email_val) && email_val.length > 0 ) {
+        errors.push(['email','Invalid Email']);
     }
 
-    // Create a user picker
-    return this.each(function() {
-        var get_user_callback = function(result, status) {
-            user = result.result;
-
-            collect='<div id="view_canvas">'+
-                        '<div class="name_plate">' +
-                            '<span id="first_name">' + user.first_name + '</span> '+
-                            '<span id="last_name">'  + user.last_name  + '</span> '+
-                        '</div>'+
-
-                        '<div class="email_plate">'+
-                            '<a id="email" class="email_link" href="mailto:'+user.email+'">' + user.email + '</a> '+
-                        '</div>'+
-                    '</div>' +
-
-                    '<div id="edit_canvas" style="display: none;">' +
-                        '<div class="name_plate_edit">'+
-                            '<input type="text" id="first_name_input" value="' + user.first_name + '" style="width: 125px;"> '+
-                            '<input type="text" id="last_name_input" value="'  + user.last_name  + '" style="width: 175px;"> '+
-                        '</div>'+
-                        '<div id="name_plate_error" class="error_plate" style="display: none;"></div>'+
-
-                        '<div class="email_plate_edit">'+
-                            '<input type="text" id="email_input" value="'  + user.email  + '" style="width: 175px;"> '+
-                        '</div>'+
-                        '<div id="email_plate_error" class="error_plate" style="display: none;"></div>'+
-                    '</div>' +
-
-                    '<div class="dates">'+
-                        '<span id="last_login">'  + user.last_login + '</span> <label>Last Logged In</label><br>'+
-                        '<span id="date_joined">' + user.date_joined + '</span> <label>Date Joined</label>'+
-                    '</div>' +
-
-                    '<div id="view_actions">'+
-                        '<a href="javascript:void(0);" id="edit_button">Edit</a>' +
-                    '</div>' +
-                    '<div id="edit_actions" style="display: none;">'+
-                        '<a href="javascript:void(0);" id="save_button">Save</a> ' +
-                        '<a href="javascript:void(0);" id="cancel_button">Cancel</a>' +
-                    '</div>';
-
-
-            container.html(collect);
-
-            // User clicks to edit it
-            $("#edit_button").click(function(){
-                change_view("edit");
-            });
-
-            // User cancels the edit
-            $("#cancel_button").click(function(){
-                change_view("view");
-
-                // Reset the input fields
-                $('input#first_name_input').val($('span#first_name').html());
-                $('input#last_name_input').val($('span#last_name').html());
-                $('input#email_input').val($('a#email').html());
-            });
-
-            // User clicks save
-            $("#save_button").click(function(e){
-                validate_and_save();
-            });
-
-        }
+    // No Errors so save the user
+    if (errors.length == 0) {
         $.getJSON("/json_api/", {"id": 1,
-                                 "method": "get_user",
-                                 "params" : JSON.stringify([user_name])
+                                 "method": "update_user",
+                                 "params" : JSON.stringify([{'user_name': this.manager.user_name,
+                                              'first_name': first_name_val,
+                                              'last_name' : last_name_val,
+                                              'email'     : email_val
+                                             }])
                                  },
-                                 get_user_callback);
-    });
+                                 function(result, status) {
+                                     // Update our user_obj to reflect the save
+                                     self.user_obj['first_name'] = first_name_val;
+                                     self.user_obj['last_name'] = last_name_val;
+                                     self.user_obj['email'] = email_val;
+
+                                     // Then draw the view screen
+                                     self.draw_view();
+                                     
+                                     // Notify others of the change
+                                     self.manager.notify_change(self.notify_name);
+                                 });
+
+    // Validation Errors. Show the user the problems
+    } else {
+        var name_errors = new Array();
+        var email_errors = new Array();
+        for (i in errors) {
+            if (errors[i][0] == 'name') name_errors.push(errors[i][1]);
+            if (errors[i][0] == 'email') email_errors.push(errors[i][1]);
+        }
+
+        if (name_errors.length > 0) {
+            this.getItem("#name_plate_error").slideDown();
+            this.getItem("#name_plate_error").html(name_errors.join(", ") + " Is Required");
+        }
+        if (email_errors.length > 0) {
+            this.getItem("#email_plate_error").slideDown();
+            this.getItem("#email_plate_error").html("Invalid Email");
+        }
+    }
 }
