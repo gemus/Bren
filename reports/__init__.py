0024d50a5d3a5558ffae82340640a73f17e605fe
owen <owenmead@gmail.com>
2010-04-28 09:49:02 -0700

    Formatting the completed workout report a bit more

--- reports/__init__.py
+++ reports/__init__.py
@@ -8,5 +8,9 @@ OUTPUT_FORMAT = "%A %B %d, %Y" # Monday December 1, 2009
 def python_date_to_display_str(python_date):
     return python_date.strftime(OUTPUT_FORMAT).replace(' 0', ' ')
 
+SHORT_OUTPUT_FORMAT = "%B %d" # December 1
+def python_date_to_short_display_str(python_date):
+    return python_date.strftime(SHORT_OUTPUT_FORMAT).replace(' 0', ' ')
+
 def get_day_of_week_str(python_date):
     return python_date.strftime("%A")
